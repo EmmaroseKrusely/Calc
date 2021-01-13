17b209782ad9f558d276803f06eb569904ca67c3
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2021-01-13 12:42:23 -0500

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+
+document.getElementById("pageTitle").innerHTML = "Emma's Coding Calculator"
+
+function mathAddition(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne + numbTwo);
+};
+
+function mathSubtraction(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne - numbTwo);
+};
+
+function mathMultiply(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne * numbTwo);
+};
+
+function mathDivision(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne / numbTwo);
+};
